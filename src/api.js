a37643d243879feb074c0297e4c7288530fb06a6
Igor <igor8212@mail.ru>
2021-08-25 22:11:23 +0300

    key removed

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
+
 export const getGames = async () => {
   const response = await fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
     "method": "GET",
